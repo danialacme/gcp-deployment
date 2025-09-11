33fa85f83f613d70e6f4198cfdcabaf58e9df19d
danial <danial.nawaz.acme.one@gmail.com>
2025-09-11 11:41:55 +0500

    This is gcp deployment project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React 🚀</h1>
+      <p>This is the simplest React app you can deploy to GCP Kubernetes.</p>
+    </div>
+  );
+}
+
+export default App;
